3d66d3d1eacb30b0bfa4fe0025ea799dbaaa9a46
biabrasil <bbia20@outlook.pt>
2023-02-13 12:36:35 +0000

    Configuring Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import { useState } from "react";
+
+import BookCreate from "./components/BookCreate";
 
 function App() {
-  return <div className="App">App</div>;
+  const [books, setBooks] = useState([]);
+  const createBook = (title) => {
+    console.log("Need to add book with title of:", title);
+  };
+  return (
+    <div className="App">
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,3 +1,31 @@
-export default function BookCreate() {
-  return <div>BookCreate</div>;
+import { useState } from "react";
+
+export default function BookCreate({ onCreate }) {
+  const [title, setTitle] = useState("");
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const hadleSubmit = (event) => {
+    event.preventDefault();
+    onCreate(title);
+    setTitle("");
+  };
+  return (
+    <div className="w-full max-w-xs bg-black">
+      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div class="mb-4">
+          <h2>Register a new book:</h2>
+          <form onSubmit={hadleSubmit}>
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Title
+            </label>
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" value={title} onChange={handleChange}></input>
+            <button>Create</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
