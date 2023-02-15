470e4f38a0167a0c007500a90804f802dc621286
biabrasil <bbia20@outlook.pt>
2023-02-15 15:14:36 +0000

    Refactoring App and adding Book Context File

--- src/App.js
+++ src/App.js
@@ -1,68 +1,24 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useEffect, useContext } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
 import NavBar from "./components/NavBar";
-import axios from "axios";
+import BooksContext from "./context/books";
 
 function App() {
-  const [books, setBooks] = useState([]);
-
-  const fetchBooks = async () => {
-    const response = await axios.get("http://localhost:3001/books");
-
-    setBooks(response.data);
-  };
+  const {fetchBooks} = useContext(BooksContext);
 
   useEffect(() => {
     fetchBooks();
   }, []);
 
-  const deleteBookById = async (id) => {
-    const response = await axios.delete(`http://localhost:3001/books/${id}`);
-
-    const updatedBooks = books.filter((book) => {
-      return book.id !== id;
-    });
-    setBooks(updatedBooks);
-  };
-
-  const editBookById = async (id, newTitle) => {
-    const response = await axios.put(`http://localhost:3001/books/${id}`, {
-      title: newTitle,
-    });
-
-    const updatedBooks = books.map((book) => {
-      if (book.id === id) {
-        return { ...book, ...response.data };
-      }
-      return book;
-    });
-    setBooks(updatedBooks);
-  };
-
-  const createBook = async (title) => {
-    const response = await axios.post("http://localhost:3001/books", {
-      title: title,
-    });
-
-    const updatedBooks = [...books, response.data];
-    setBooks(updatedBooks);
-
-    console.log(response);
-  };
-
   return (
     <div>
       <NavBar />
       <div className="App">
-        <BookCreate onCreate={createBook} />
+        <BookCreate />
         <div className="mt-10">
-          <BookList
-            books={books}
-            onDelete={deleteBookById}
-            onEdit={editBookById}
-          />
+          <BookList />
         </div>
       </div>
     </div>
