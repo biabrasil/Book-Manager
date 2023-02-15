638a34a7f497def0051bcc69da45477b8508d130
biabrasil <bbia20@outlook.pt>
2023-02-15 15:21:16 +0000

    Refactoring Book List Component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,10 +1,11 @@
 import BookShow from "./BookShow";
+import { useContext } from "react";
+import BooksContext from "../context/books";
 
-export default function BookList({ books, onDelete, onEdit }) {
+export default function BookList() {
+  const { books } = useContext(BooksContext);
   const renderedBooks = books.map((book) => {
-    return (
-      <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
-    );
+    return <BookShow key={book.id} book={book} />;
   });
   return <div>{renderedBooks}</div>;
 }
